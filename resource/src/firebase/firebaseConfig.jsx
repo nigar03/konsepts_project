265a9359ccf9966b3a73885c6e1c8355b73f8a05
nigar03 <125979720+nigar03@users.noreply.github.com>
2024-03-17 21:33:46 +0400

    Update firebaseConfig.jsx

--- resource/src/firebase/firebaseConfig.jsx
+++ resource/src/firebase/firebaseConfig.jsx
@@ -2,13 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/database'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2_gcPRDeLwZYgbtMxIgPMx5PpdJfbDhw",
-  authDomain: "konsept-3.firebaseapp.com",
-  databaseURL: "https://konsept-3-default-rtdb.firebaseio.com",
-  projectId: "konsept-3",
-  storageBucket: "konsept-3.appspot.com",
-  messagingSenderId: "661367738133",
-  appId: "1:661367738133:web:08df0cf64e3afd45ade31c"
+ apiKey: "AIzaSyA8_nu6XB6TsJYkWoU7bxCrwQjU3_Hm914",
+  authDomain: "kons-cb1b9.firebaseapp.com",
+  databaseURL: "https://kons-cb1b9-default-rtdb.firebaseio.com",
+  projectId: "kons-cb1b9",
+  storageBucket: "kons-cb1b9.appspot.com",
+  messagingSenderId: "75150828319",
+  appId: "1:75150828319:web:2a7ae1f90a12ec6853c62a",
+  measurementId: "G-DG1GLBV90S"
   };
 
 // Initialize Firebase
